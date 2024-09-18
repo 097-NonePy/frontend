56256fb8c5adfc8c484886ba791083075c583004
Savith Panamgama <geethikasavith@gmail.com>
2024-09-18 21:45:38 +0530

    text

--- src/sections/chat/chat-view/ChatHeader.tsx
+++ src/sections/chat/chat-view/ChatHeader.tsx
@@ -15,7 +15,13 @@ export const ChatHeader = ({
     padding="10px"
     height="50px"
   >
-    <Typography variant="h6">ChatBot</Typography>
+    <Typography
+      variant="h6"
+      sx={{ fontSize: '16px', padding: '0px 25px', textAlign: 'center' }}
+    >
+      Ask any question about candidates, their promises, and election details. Our
+      AI-powered Election Bot has you covered!
+    </Typography>
     <IconButton onClick={onMenuClick}>
       <Icon icon="mdi:menu" width="24" height="24" />
     </IconButton>
